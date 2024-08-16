cb7c805bfe869c59d4f597cc2a9d9dfd95d7d067
jmjcoding <jmjcoding@icloud.com>
2024-08-16 14:36:53 -0500

    feat: create new database table for company contact information

--- migrations/20240330041510_/migration.sql
+++ migrations/20240330041510_/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE `User` (
+CREATE TABLE IF NOT EXISTS `User` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL DEFAULT '',
     `email` VARCHAR(191) NOT NULL DEFAULT '',
@@ -8,6 +8,40 @@ CREATE TABLE `User` (
     `createdAt` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
     `lastLoginDate` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
 
+
     UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `ees-contact-info` (
+    `CONTACT_INFO_ID` INT PRIMARY KEY,
+    `EMAIL` VARCHAR(255),
+    `PHONE` VARCHAR(255),
+    `ADDRESS` VARCHAR(255),
+    `CITY` VARCHAR(255),
+    `STATE` VARCHAR(255),
+    `ZIP` VARCHAR(255),
+    `COUNTRY` VARCHAR(255)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+INSERT INTO
+    `ees-contact-info` (
+        `CONTACT_INFO_ID`,
+        `EMAIL`,
+        `PHONE`,
+        `ADDRESS`,
+        `CITY`,
+        `STATE`,
+        `ZIP`,
+        `COUNTRY`
+    )
+VALUES (
+        1,
+        'email',
+        '555-555-5555',
+        'address',
+        'city',
+        'state',
+        'zip',
+        'country'
+    );
