b782b21b7b5e2349c2e982e1dd68e047bb4ddcfe
jmjcoding <joshuarcher76@gmail.com>
2024-08-17 10:00:55 -0500

    Create database table for EES contact information

--- migrations/20240330041510_/migration.sql
+++ migrations/20240330041510_/migration.sql
@@ -12,36 +12,3 @@ CREATE TABLE IF NOT EXISTS `User` (
     UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
-CREATE TABLE IF NOT EXISTS `ees-contact-info` (
-    `CONTACT_INFO_ID` INT PRIMARY KEY,
-    `EMAIL` VARCHAR(255),
-    `PHONE` VARCHAR(255),
-    `ADDRESS` VARCHAR(255),
-    `CITY` VARCHAR(255),
-    `STATE` VARCHAR(255),
-    `ZIP` VARCHAR(255),
-    `COUNTRY` VARCHAR(255)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
-INSERT INTO
-    `ees-contact-info` (
-        `CONTACT_INFO_ID`,
-        `EMAIL`,
-        `PHONE`,
-        `ADDRESS`,
-        `CITY`,
-        `STATE`,
-        `ZIP`,
-        `COUNTRY`
-    )
-VALUES (
-        1,
-        'email',
-        '555-555-5555',
-        'address',
-        'city',
-        'state',
-        'zip',
-        'country'
-    );
