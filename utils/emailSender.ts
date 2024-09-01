b4b8ea46a478b47f710667501b1d33f4a50eb6a2
RuvenPinkhasov <rubi00010@gmail.com>
2024-09-01 17:07:47 -0400

    feat: created emailSender.ts

---
+++ utils/emailSender.ts
@@ -0,0 +1,28 @@
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: parseInt(process.env.SMTP_PORT as string, 10),
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+export const sendEmail = async (to: string, from: string, subject: string, text: string) => {
+  const mailOptions = {
+    from,
+    to,
+    subject,
+    text,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log('Email sent successfully');
+  } catch (error) {
+    console.error('Error sending email:', error);
+    throw new Error('Could not send email');
+  }
+};
