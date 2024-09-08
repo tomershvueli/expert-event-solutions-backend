e073486e56f4c769a1a6649e4e8dbe61f2ba7c26
RuvenPinkhasov <rubi00010@gmail.com>
2024-09-08 16:56:41 -0400

    feat: created send email endpoint

--- keystone.ts
+++ keystone.ts
@@ -1,10 +1,12 @@
 import dotenv from "dotenv";
 import { config } from "@keystone-6/core";
+import express from "express";
 
 dotenv.config();
 
 import { withAuth, session } from "./auth";
 import * as Models from "./models";
+import { sendEmail } from "./utils/emailSender";
 
 export default withAuth(
   config({
@@ -13,6 +15,29 @@ export default withAuth(
       cors: {
         origin: "*",
         methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      },
+      extendExpressApp: (app, commonContext) => {
+        app.use(express.json());
+        app.post("/sendEmail", async (req, res) => {
+          const emailText = req.body.phoneNumber
+            ? `User provided phone number: ${req.body.phoneNumber}\n${req.body.text}`
+            : req.body.text;
+          try {
+            await sendEmail(
+              process.env.EMAIL_TO as string,
+              process.env.SMTP_USER as string,
+              req.body.subject,
+              emailText,
+              req.body.replyTo
+            );
+            res.status(200).json({ success: true });
+          } catch (error) {
+            console.error("Error sending email:", error);
+            res
+              .status(500)
+              .json({ success: false, error: "Failed to send email" });
+          }
+        });
       },
     },
     db: {
@@ -33,7 +58,8 @@ export default withAuth(
       s3_file_storage: {
         kind: "s3",
         type: "file",
-        bucketName: process.env.S3_BUCKET_NAME || "expert-event-solutions-keystonejs",
+        bucketName:
+          process.env.S3_BUCKET_NAME || "expert-event-solutions-keystonejs",
         region: process.env.S3_REGION || "us-east-2",
         accessKeyId: process.env.S3_ACCESS_KEY_ID || "keystone",
         secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || "keystone",
@@ -43,7 +69,8 @@ export default withAuth(
       s3_image_storage: {
         kind: "s3",
         type: "image",
-        bucketName: process.env.S3_BUCKET_NAME || "expert-event-solutions-keystonejs",
+        bucketName:
+          process.env.S3_BUCKET_NAME || "expert-event-solutions-keystonejs",
         region: process.env.S3_REGION || "us-east-2",
         accessKeyId: process.env.S3_ACCESS_KEY_ID || "keystone",
         secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || "keystone",
@@ -53,5 +80,5 @@ export default withAuth(
     },
     lists: Models,
     session,
-  }),
+  })
 );
---
+++ utils/emailSender.ts
@@ -0,0 +1,36 @@
+import nodemailer from "nodemailer";
+require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: parseInt(process.env.SMTP_PORT as string, 10),
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+export const sendEmail = async (
+  from: string,
+  to: string,
+  subject: string,
+  text: string,
+  replyTo: string
+) => {
+  const mailOptions = {
+    from,
+    to,
+    subject,
+    text,
+    replyTo,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw new Error("Could not send email");
+  }
+};
